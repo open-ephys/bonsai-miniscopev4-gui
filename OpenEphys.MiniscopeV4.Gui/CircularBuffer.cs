718735019ef812d5d66ece818ec60f8288224e04
bparks13 <bap@open-ephys.org>
2026-07-27 17:32:34 -0400

    Replace rolling data with fixed time axis
    - Add CircularBuffer and related files
    - Expose an End value from the circular buffer to know what index is currently the newest value inserted
    - Change the time-series plots to force an x-axis that shows the entire buffer size immediately, instead of growing with time
    - Plot a vertical yellow line at the newest index inserted to the time-series data, in all time-series plots

---
+++ OpenEphys.MiniscopeV4.Gui/CircularBuffer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace OpenEphys.MiniscopeV4.Gui;
+
+class CircularBuffer<T> : IReadOnlyCollection<T>
+{
+    private readonly T[] buffer;
+
+    public CircularBuffer(int capacity)
+    {
+        buffer = new T[capacity];
+    }
+
+    private int start;
+    private int end;
+    private int count;
+
+    public ref T this[int index]
+    {
+        get => ref buffer[index % buffer.Length];
+    }
+
+    public int Count => count;
+
+    public int Start => start;
+
+    public int End => end;
+
+    public void Push(T item)
+    {
+        buffer[end] = item;
+        end = (end + 1) % buffer.Length;
+        if (count == buffer.Length)
+            start = (start + 1) % buffer.Length;
+        else
+            count++;
+    }
+
+    static void CopyList<TSource>(IReadOnlyList<TSource> source, int index, TSource[] buffer, int bufferIndex, int count)
+    {
+        if (source is TSource[] array)
+            Array.Copy(array, index, buffer, bufferIndex, count);
+        else if (source is List<TSource> list)
+            list.CopyTo(index, buffer, bufferIndex, count);
+        else
+            for (int i = index, bi = bufferIndex; i < count; i++, bi++)
+                buffer[bi] = source[i];
+    }
+
+    public void Push(IReadOnlyList<T> items)
+    {
+        var nextIndex = end + items.Count;
+        var trimOffset = (items.Count / buffer.Length) * buffer.Length;
+        start = (start + items.Count - Math.Min(items.Count, buffer.Length - count)) % buffer.Length;
+        count = Math.Min(count + items.Count, buffer.Length);
+
+        var index = end;
+        end = nextIndex % buffer.Length;
+        if (end <= index)
+        {
+            CopyList(items, trimOffset, buffer, index, buffer.Length - index);
+            trimOffset += buffer.Length - index;
+            index = 0;
+        }
+
+        CopyList(items, trimOffset, buffer, index, items.Count - trimOffset);
+    }
+
+    public void Push(IEnumerable<T> items)
+    {
+        if (items is IReadOnlyList<T> list)
+            Push(list);
+        else
+            foreach (var item in items)
+                Push(item);
+    }
+
+    public void CopyTo(T[] array)
+    {
+        var startSegmentLength = buffer.Length - start;
+        Array.Copy(buffer, start, array, 0, startSegmentLength);
+        if (startSegmentLength < buffer.Length)
+            Array.Copy(buffer, 0, array, startSegmentLength, buffer.Length - startSegmentLength);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            yield return this[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ OpenEphys.MiniscopeV4.Gui/CircularPlotPointSeries.cs
@@ -0,0 +1,41 @@
+﻿using Bonsai.ImGui.Visualizers;
+using Bonsai.ImPlot;
+using Hexa.NET.ImPlot;
+using System;
+
+namespace OpenEphys.MiniscopeV4.Gui;
+
+/// <summary>
+/// Provides an adapter over a circular buffer used for providing data for
+/// interactive visualizations.
+/// </summary>
+/// <typeparam name="TSource">The type of the elements stored in the circular buffer.</typeparam>
+public class CircularPlotPointSeries<TSource> : IPlotPointGetter, IPlotPointGetterSeries
+{
+    private readonly CircularBuffer<TSource> storage;
+    private readonly NamedPlotPointGetter[] series;
+
+    internal CircularPlotPointSeries(CircularBuffer<TSource> buffer, NamedPlotPointGetter[] getters)
+    {
+        storage = buffer;
+        series = getters;
+    }
+
+    /// <inheritdoc/>
+    public int Count => storage.Count;
+
+    /// <summary>
+    /// Gets the index of the start of the buffer.
+    /// </summary>
+    public int Start => storage.Start;
+
+    /// <summary>
+    /// Gets the index of the end of the buffer.
+    /// </summary>
+    public int End => storage.End;
+
+    /// <inheritdoc/>
+    public ReadOnlySpan<NamedPlotPointGetter> Series => series;
+
+    ImPlotPointGetter IPlotPointGetter.Getter => series[0].Getter;
+}
